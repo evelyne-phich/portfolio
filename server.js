1b99d23c60bb93542996f7811d50e6811670292c
Lyneeevy <evelyne.phich@gmail.com>
2021-02-10 19:01:59 +0100

    create resources for careers

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { careers } = require("./resources/careers");
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -23,98 +24,20 @@ app.get("/", (request, response) => {
 });
 
 // Parcours professionnel
-
-app.get("/career/5", (request, response) => {
-  response.render("career", {
-    date: "09/2019 - Aujourd'hui",
-    companyName: "LCL",
-    companyNameColor: "rgb(248, 194, 15)",
-    jobName: "Analyste comptable",
-    jobTasks: [
-      "Fiabilisation des alimentations du système comptable",
-      "Écritures d'inventaire et corrections suite à d'éventuels dysfonctionnements",
-      "Clôture mensuelle avec analyse des variations (résultat/bilan)",
-      "Contrôle comptable, justification des comptes et surveillance des suspens",
-      "Participation à des projets comptables/réglementaires (ex : IFRS 16)",
-    ],
-    activePageNumber: 5,
-    activeCareerBackground: "/css/career-5.css",
-    activePage: "Parcours professionnel",
+for (let career of careers) {
+  app.get(career.url, (request, response) => {
+    response.render(career.page, {
+      date: career.date,
+      companyName: career.companyName,
+      companyNameColor: career.companyNameColor,
+      jobName: career.jobName,
+      jobTasks: career.jobTasks,
+      activePageNumber: career.activePageNumber,
+      activeCareerBackground: career.activeCareerBackground,
+      activePage: career.activePage,
+    });
   });
-});
-app.get("/career/4", (request, response) => {
-  response.render("career", {
-    date: "10/2017 - 09/2019",
-    companyName: "Fiducial Expertise",
-    companyNameColor: "rgb(187, 184, 184)",
-    jobName: "Collaboratrice comptable",
-    jobTasks: [
-      "Gestion d’un portefeuille d’une trentaine de clients",
-      "Déplacements mensuels en clientèle",
-      "Tenue comptable et déclarations fiscales",
-      "Etablissement des comptes annuels et des liasses fiscales",
-      "Etablissement d’états intermédiaires et de prévisionnels",
-      "Déclarations d’IRPP (Impôt sur le Revenu des Personnes Physiques)",
-    ],
-    activePageNumber: 4,
-    activeCareerBackground: "/css/career-4.css",
-    activePage: "Parcours professionnel",
-  });
-});
-app.get("/career/3", (request, response) => {
-  response.render("career", {
-    date: "11/2016 - 10/2017",
-    companyName: "Groupe Y Nexia",
-    companyNameColor: "rgb(24, 207, 207)",
-    jobName: "Assistante comptable",
-    jobTasks: [
-      "Déplacements mensuels en clientèle",
-      "Tenue comptable et déclarations fiscales",
-      "Etablissement des comptes annuels et des liasses fiscales",
-    ],
-    activePageNumber: 3,
-    activeCareerBackground: "/css/career-3.css",
-    activePage: "Parcours professionnel",
-  });
-});
-app.get("/career/2", (request, response) => {
-  response.render("career", {
-    date: "09/2014 - 08/2016",
-    companyName: "Société Générale",
-    companyNameColor: "rgb(235, 28, 28)",
-    jobName: "Contrôleuse de gestion",
-    jobTasks: [
-      "Suivi des frais généraux et de la facturation interne",
-      "Participation aux clôtures mensuelles",
-      "Participation à l’établissement des estimés trimestriels et du budget annuel",
-    ],
-    school: "IAE Gustave Eiffel",
-    degree:
-      "Master Comptabilité, Contrôle, Audit en apprentissage - Mention bien",
-    activePageNumber: 2,
-    activeCareerBackground: "/css/career-2.css",
-    activePage: "Parcours professionnel",
-  });
-});
-app.get("/career/1", (request, response) => {
-  response.render("career", {
-    date: "10/2013 - 08/2014",
-    companyName: "Siemens",
-    companyNameColor: "rgb(27, 209, 209)",
-    jobName: "Analyste crédit clients",
-    jobTasks: [
-      "Gestion et analyse d’un portefeuille de comptes clients",
-      "Participation au recouvrement et à l’amélioration du DSO <i>(Days Sales Outstanding)</i>",
-      "Suivi des litiges et remises de chèques",
-    ],
-    school: "IAE Gustave Eiffel",
-    degree:
-      "Licence Comptabilité, Contrôle, Audit en apprentissage - Mention bien",
-    activePageNumber: 1,
-    activeCareerBackground: "/css/career-1.css",
-    activePage: "Parcours professionnel",
-  });
-});
+}
 
 // Projets personnels
 
