48f1bab52d2a4b332eec0f69ff712b71aa5e544f
evelyne-phich <evelyne.phich@gmail.com>
2022-08-07 16:32:48 +0200

    Revert "clean repo"
    This reverts commit 2a785d8510d58808b1f6e68f71c34ab46eabcb61.

---
+++ app/router.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { careers, projects } = require("../database/data");
+
+// Accueil
+router.get("/", (request, response) => {
+  response.render("index", {
+    fullName: "Evelyne PHICH",
+    job: "Développeuse fullstack JS",
+    activePage: "Accueil",
+  });
+});
+
+// Parcours professionnel
+router.get("/careers", (request, response) => {
+  response.render("careers", {
+    careers: careers,
+    activePage: "Parcours professionnel",
+  });
+});
+
+// Projets personnels
+router.get("/projects", (request, response) => {
+  response.render("projects", {
+    projects: projects,
+    activePage: "Projets personnels",
+  });
+});
+
+module.exports = router;
---
+++ database/data.js
@@ -0,0 +1,114 @@
+const careers = [
+  {
+    date: "08/2021 - 02/2022",
+    companyName: "O'clock",
+    companyNameColor: "#09B99E",
+    jobName: "Formation développeur fullstack JS",
+    jobTasks: [
+      "3 mois de socle :",
+      "HTML - CSS - JavaScript - Node.js - Postgres - Git - méthodes Agile - mise en production",
+      "1 mois de spécialisation :",
+      "React",
+      "1 mois de projet professionnel en équipe :",
+    ],
+    project: "O'My Plant",
+  },
+  {
+    date: "09/2019 - 06/2021",
+    companyName: "LCL",
+    companyNameColor: "rgb(248, 194, 15)",
+    jobName: "Analyste comptable",
+    jobTasks: [
+      "Fiabilisation des alimentations du système comptable",
+      "Écritures d'inventaire et corrections suite à d'éventuels dysfonctionnements",
+      "Clôture mensuelle avec analyse des variations (résultat/bilan)",
+      "Contrôle comptable, justification des comptes et surveillance des suspens",
+      "Participation à des projets comptables/réglementaires (ex : IFRS 16)",
+    ],
+  },
+  {
+    date: "10/2017 - 09/2019",
+    companyName: "Fiducial Expertise",
+    companyNameColor: "rgb(187, 184, 184)",
+    jobName: "Collaboratrice comptable",
+    jobTasks: [
+      "Gestion d’un portefeuille d’une trentaine de clients",
+      "Déplacements mensuels en clientèle",
+      "Tenue comptable et déclarations fiscales",
+      "Etablissement des comptes annuels et des liasses fiscales",
+      "Etablissement d’états intermédiaires et de prévisionnels",
+      "Déclarations d’IRPP (Impôt sur le Revenu des Personnes Physiques)",
+    ],
+  },
+  {
+    date: "11/2016 - 10/2017",
+    companyName: "Groupe Y Nexia",
+    companyNameColor: "rgb(8 173 173)",
+    jobName: "Assistante comptable",
+    jobTasks: [
+      "Déplacements mensuels en clientèle",
+      "Tenue comptable et déclarations fiscales",
+      "Etablissement des comptes annuels et des liasses fiscales",
+    ],
+  },
+  {
+    date: "09/2014 - 08/2016",
+    companyName: "Société Générale",
+    companyNameColor: "rgb(235, 28, 28)",
+    jobName: "Contrôleuse de gestion",
+    jobTasks: [
+      "Suivi des frais généraux et de la facturation interne",
+      "Participation aux clôtures mensuelles",
+      "Participation à l’établissement des estimés trimestriels et du budget annuel",
+    ],
+    school: "IAE Gustave Eiffel",
+    degree:
+      "Master Comptabilité, Contrôle, Audit en apprentissage - Mention bien",
+  },
+  {
+    date: "10/2013 - 08/2014",
+    companyName: "Siemens",
+    companyNameColor: "rgb(8 173 173)",
+    jobName: "Analyste crédit clients",
+    jobTasks: [
+      "Gestion et analyse d’un portefeuille de comptes clients",
+      "Participation au recouvrement et à l’amélioration du DSO (Days Sales Outstanding)",
+      "Suivi des litiges et remises de chèques",
+    ],
+    school: "IAE Gustave Eiffel",
+    degree:
+      "Licence Comptabilité, Contrôle, Audit en apprentissage - Mention bien",
+  },
+];
+
+const projects = [
+  {
+    date: "01/2022",
+    projectName: "Chatroom",
+    projectNameColor: "#0F4C75",
+    linkToProject: "https://chatroom-d-evy.herokuapp.com/",
+  },
+  {
+    date: "12/2021",
+    projectName: "To-do list",
+    projectNameColor: "#1A5D4A",
+    linkToProject: "https://to-do-list-d-evy.surge.sh/",
+  },
+  {
+    date: "10/2021",
+    projectName: "Les bonnes recettes d'Evy",
+    projectNameColor: "rgb(182, 7, 65)",
+    linkToProject: "https://les-bonnes-recettes-d-evy.herokuapp.com/",
+  },
+  {
+    date: "11/2020",
+    projectName: "Les bonnes adresses d'Evy",
+    projectNameColor: "rgb(241, 97, 97)",
+    linkToProject: "https://les-bonnes-adresses-d-evy.herokuapp.com/",
+  },
+];
+
+module.exports = {
+  careers,
+  projects,
+};
