47818c7452fa9d264cc2042069dea94aabb379cc
evelyne-phich <evelyne.phich@gmail.com>
2022-08-10 21:13:06 +0200

    update AboutMe text

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -6,7 +6,7 @@ import { ArrowDown } from "components/Icons/ArrowDown";
 
 export const AboutMe = () => {
   return (
-    <>
+    <div id="aboutMe">
       <Title>ABOUT ME</Title>
       <img
         src="https://res.cloudinary.com/dvpcjyb48/image/upload/c_scale,w_300/v1660056581/IMG_20220805_233419_026_c3acff.jpg"
@@ -17,15 +17,23 @@ export const AboutMe = () => {
         Hello !
         <br />I am <strong>Evelyne PHICH</strong>.
         <br />
-        I used to work in accounting for 8 years, until mid 2021, when I decided
-        to change path and start a new life as a software engineer.
+        I used to work in accounting for 8 years in various companies and
+        different positions, but I've always been attracted to software
+        development.
         <br />
-        If you're interested to know more about me, I invite you to scroll down.
-        🙂
+        In 2021 I felt it was the right time to set myself this challenge :
+        <br />
+        changing path and starting a new journey as a software engineer.
+        <br />
       </Text>
-      <div className={style.doubleArrowDown}>
-        <ArrowDown />
+      <div>
+        <Text>Scroll down to know more about me !</Text>
+        <div className={style.arrowDownContainer}>
+          <div className={style.arrowDown}>
+            <ArrowDown />
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
