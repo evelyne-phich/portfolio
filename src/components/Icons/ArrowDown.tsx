42ca56533e0fd84aff4ac4c62592577561a067dd
evelyne-phich <evelyne.phich@gmail.com>
2022-08-11 20:23:54 +0200

    fix layout

--- src/components/Icons/ArrowDown.tsx
+++ src/components/Icons/ArrowDown.tsx
@@ -4,7 +4,7 @@ export const ArrowDown = () => {
       <path d="M0 0h24v24H0V0z" fill="none" />
       <path
         d="m20 12-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"
-        fill="#21273D"
+        fill="#5584ac"
       />
     </svg>
   );
