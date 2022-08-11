9d0789d072fbba91db7bec74835b4e18acb10ace
evelyne-phich <evelyne.phich@gmail.com>
2022-08-11 18:19:53 +0200

    add experiences in MyCareer

--- src/components/CareerCard/index.tsx
+++ src/components/CareerCard/index.tsx
@@ -4,15 +4,28 @@ import { ReactNode } from "react";
 type Props = {
   title: string;
   position: string;
+  title2?: string;
+  position2?: string;
+  detail?: string;
   children?: ReactNode;
 };
 
-export const CareerCard = ({ title, position, children }: Props) => {
+export const CareerCard = ({
+  title,
+  position,
+  title2,
+  position2,
+  detail,
+  children,
+}: Props) => {
   return (
     <div className={style.careerCard}>
       <h3 className={style.title}>{title}</h3>
       <p className={style.position}>{position}</p>
       <ul className={style.description}>{children}</ul>
+      <h3 className={style.title}>{title2}</h3>
+      <p className={style.position}>{position2}</p>
+      <p className={style.detail}>{detail}</p>
     </div>
   );
 };
