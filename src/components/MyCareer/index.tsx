8c59b805225eb7daf198ec8d9159fce5d522a914
evelyne-phich <evelyne.phich@gmail.com>
2022-08-11 18:19:53 +0200

    add experiences in MyCareer

--- src/components/MyCareer/index.tsx
+++ src/components/MyCareer/index.tsx
@@ -49,7 +49,58 @@ export const MyCareer = () => {
         </CareerCard>
         <h2 className={style.date}>08/2021</h2>
         <h2 className={style.date}>06/2021</h2>
-        <CareerCard title="LCL" position="Accountant analyst" />
+        <CareerCard title="LCL" position="Accountant analyst">
+          <li>Inventory entries and corrections</li>
+          <li>
+            Monthly closing with analysis of variations (results/balance sheet)
+          </li>
+          <li>Accounting control and accounts justification</li>
+          <li>Contribution to IFRS16 accounting and regulatory project</li>
+        </CareerCard>
+        <h2 className={style.date}>09/2019</h2>
+        <CareerCard title="Fiducial Expertise" position="Accountant">
+          <li>Management of a portfolio of about thirty clients</li>
+          <li>Bookkeeping and tax declarations</li>
+          <li>Establishment of annual accounts and tax returns</li>
+          <li>Establishment of intermediate statements and forecasts</li>
+          <li>Personal income tax declarations</li>
+        </CareerCard>
+        <h2 className={style.date}>10/2017</h2>
+        <CareerCard title="Groupe Y Nexia" position="Accounting assistant">
+          <li>Bookkeeping and tax declarations</li>
+          <li>Establishment of annual accounts and tax returns</li>
+        </CareerCard>
+        <h2 className={style.date}>11/2016</h2>
+        <h2 className={style.date}>08/2016</h2>
+        <CareerCard
+          title="Société Générale"
+          position="Business controller"
+          title2="IAE Gustave Eiffel"
+          position2={`"Comptabilité, Contrôle, Audit" Master's degree (apprenticeship)`}
+          detail="with high honours"
+        >
+          <li>Monitoring of general expenses and internal invoicing</li>
+          <li>Contribution to monthly closings</li>
+          <li>
+            Contribution to the establishment of quarterly estimates and the
+            annual budget
+          </li>
+        </CareerCard>
+        <h2 className={style.date}>09/2014</h2>
+        <CareerCard
+          title="Siemens"
+          position="Credit manager"
+          title2="IAE Gustave Eiffel"
+          position2={`"Comptabilité, Contrôle, Audit" Bachelor's degree (apprenticeship)`}
+          detail="with high honours"
+        >
+          <li>Management and analysis of a portfolio of customer accounts</li>
+          <li>
+            Contribution to collection and improvement of the DSO (Days Sales
+            Outstanding)
+          </li>
+          <li>Monitoring of disputes and management of check deposit</li>
+        </CareerCard>
       </div>
     </Section>
   );
