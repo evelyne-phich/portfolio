c9d6e66f008d23dc3b19406f09586dd905685e18
evelyne-phich <evelyne.phich@gmail.com>
2022-08-12 12:51:52 +0200

    fix Navbar and AboutMe layout and responsive

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -7,25 +7,29 @@ import { ArrowDownIcon } from "components/Icons/ArrowDownIcon";
 export const AboutMe = () => {
   return (
     <Section id="aboutMe" title="ABOUT ME">
-      <img
-        src="https://res.cloudinary.com/dvpcjyb48/image/upload/c_scale,w_300/v1660056581/IMG_20220805_233419_026_c3acff.jpg"
-        alt="Me"
-        className={style.image}
-      />
-      <Text>
-        Hello !
-        <br />
-        I'm <strong>Evelyne PHICH</strong>.
-        <br />
-        I used to work in accounting for 8 years for various companies and in
-        several positions, but I've always been attracted to software
-        development.
-        <br />
-        In 2021 I felt it was the right time to set myself this challenge :
-        <br />
-        changing path and starting a new journey as a software engineer.
-        <br />
-      </Text>
+      <div className={style.container}>
+        <img
+          src="https://res.cloudinary.com/dvpcjyb48/image/upload/c_scale,w_300/v1660056581/IMG_20220805_233419_026_c3acff.jpg"
+          alt="Me"
+          className={style.image}
+        />
+        <div className={style.textContainer}>
+          <Text>
+            Hello !
+            <br />
+            I'm <strong>Evelyne PHICH</strong>.
+            <br />
+            I used to work in accounting for 8 years for various companies and
+            in several positions, but I've always been attracted to software
+            development.
+            <br />
+            In 2021 I felt it was the right time to set myself this challenge :
+            <br />
+            changing path and starting a new journey as a software engineer.
+            <br />
+          </Text>
+        </div>
+      </div>
       <div className={style.scrollDownContainer}>
         <Text>Scroll down to know more about me !</Text>
         <div className={style.arrowDownContainer}>
