c9fb466428499d4b00e82672a563103a4a795015
evelyne-phich <evelyne.phich@gmail.com>
2022-08-12 11:37:17 +0200

    add animation on CareerCard

--- src/components/CareerCard/index.tsx
+++ src/components/CareerCard/index.tsx
@@ -1,5 +1,8 @@
 import style from "./index.module.scss";
-import { ReactNode } from "react";
+
+import { ReactNode, useEffect, useRef, useState } from "react";
+
+const options = { root: null, rootMargin: "0px", threshold: 1 };
 
 type Props = {
   title: string;
@@ -18,16 +21,40 @@ export const CareerCard = ({
   detail,
   children,
 }: Props) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const startAnimation: IntersectionObserverCallback = (entries) => {
+    setIsVisible(entries[0].isIntersecting);
+  };
+
+  const careerCardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(startAnimation, options);
+
+    if (careerCardRef.current) {
+      observer.observe(careerCardRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <div className={style.careerCard}>
-      <h3 className={style.title}>{title}</h3>
-      <p className={style.position}>{position}</p>
-      <ul className={style.description}>{children}</ul>
-      {title2 && (
-        <h3 className={`${style.title} ${style.withSpacing}`}>{title2}</h3>
-      )}
-      {position2 && <p className={style.position}>{position2}</p>}
-      {detail && <p className={style.detail}>{detail}</p>}
+    <div className={style.container} ref={careerCardRef}>
+      <div
+        className={`${style.careerCard} ${isVisible ? style.isVisible : ""}`}
+      >
+        <h3 className={style.title}>{title}</h3>
+        <p className={style.position}>{position}</p>
+        <ul className={style.description}>{children}</ul>
+        {title2 && (
+          <h3 className={`${style.title} ${style.withSpacing}`}>{title2}</h3>
+        )}
+        {position2 && <p className={style.position}>{position2}</p>}
+        {detail && <p className={style.detail}>{detail}</p>}
+      </div>
     </div>
   );
 };
